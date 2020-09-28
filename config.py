71db0ac8c1ea6533d3497716dcd191db15b0644e
kilewa <kilewageorge230@gmail.com>
2020-09-28 15:27:15 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kilewa:george@localhost/blog"
     UPLOADED_PHOTOS_DEST = "app/static/photos"
 
     # email configurations
